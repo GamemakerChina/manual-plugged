6f3f7534cb90a8d52b2649c422950a49b534b627
LiarOnce <qq1062787843@hotmail.com>
2023-09-20 14:28:01 +0000

    Deploying to main from @ GamemakerChina/GameMaker-maunl-zh@a26c49bd5da6cd161ff7b812082e0ea23ac8af9f 🚀

--- whxdata/idx.new.js
+++ whxdata/idx.new.js
@@ -1,4 +1,4 @@
 (function() {
-var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"-","last":"zip_unzip","num":"3345","node":"idata1"}]};
+var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"-","last":"zip_unzip_async","num":"3507","node":"idata1"}]};
 window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), index, { sync:true });
 })();
---
+++ whxdata/toc197.new.js
@@ -0,0 +1,1 @@
+(function() {    var toc =  [{"name":"wallpaper_set_config","type":"item","tilePath":"","description":"","url":"GameMaker_Language/GML_Reference/Live_Wallpapers/wallpaper_set_config.htm"}];    window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });    })();
